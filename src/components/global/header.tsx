3ee99ebcfef8d28e8174f6d72849ef903e2c95c0
kadolfo9 <kaykyadolfo21@hotmail.com>
2025-06-03 00:04:52 -0300

    refactor: using link instead

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "@/hooks/use-auth";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 
 export function Header() {
   const auth = useAuth();
@@ -12,14 +13,20 @@ export function Header() {
     navigate("/", { replace: true, viewTransition: true });
   };
 
+  useEffect(() => {}, [navigate]);
+
   const bindLinks = () => {
     const commonLinks =
       <>
         <li>
-          <Button variant="secondary" onClick={() => navigate("/")}>Home</Button>
+          <Link to="/">
+            <Button variant="secondary">Home</Button>
+          </Link>
         </li>
         <li>
-          <Button variant="secondary" onClick={() => navigate("/about")}>Sobre</Button>
+          <Link to="/about">
+            <Button variant="secondary">Sobre</Button>
+          </Link>
         </li>
       </>
     
@@ -27,9 +34,11 @@ export function Header() {
       return <>
         { commonLinks }
         <li>
-          <Button variant="secondary" onClick={() => navigate("/profile")}>Minhas Publicações</Button>
+          <Link to="/profile">
+            <Button variant="secondary">Minhas Publicações</Button>
+          </Link>
         </li>
-        <li className="place-content-end">
+        <li className="place-content-end p-1">
           <Button variant="destructive" onClick={handleLogout}>Sair</Button>
         </li>
       </>
@@ -37,7 +46,9 @@ export function Header() {
       return <>
         { commonLinks }
         <li className="place-content-end">
-          <Button variant="secondary" onClick={() => navigate('/auth')}>Login</Button>
+          <Link to="/auth">
+            <Button variant="secondary">Login</Button>
+          </Link>
         </li>
       </>
     }
@@ -45,7 +56,7 @@ export function Header() {
 
   return (
     <nav className="center bg-gray-100 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
         <ul className="menu">
           { bindLinks() }
         </ul>
